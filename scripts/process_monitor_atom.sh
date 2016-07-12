5336dfd583dccee62764211e6da3e07082f37a4f
Zhicheng <Zhicheng_qiu@cable.comcast.com>
2016-07-11 17:47:52 -0700

    RDKB-6612:
    Reason for change: ATOM self-healing code reorg
    Test Procedure: ATOM log
    Risks: None
    Signed-off-by: Zhicheng <Zhicheng_qiu@cable.comcast.com>
    
    Change-Id: I1e381850327b7b0d064e325cf6f3d1d1122294b7

--- scripts/process_monitor_atom.sh
+++ scripts/process_monitor_atom.sh
@@ -10,29 +10,39 @@ do
 	sleep 300
 	cd /usr/ccsp/wifi
 	WiFi_PID=`pidof CcspWifiSsp`
-	if [ "$WiFi_PID" = "" ]; then
+	if [ "$WiFi_PID" == "" ]; then
 		echo "WiFi process is not running, restarting it"
 		export LD_LIBRARY_PATH=$PWD:.:$PWD/../../lib:$PWD/../../.:/lib:/usr/lib:$LD_LIBRARY_PATH
                 dmcli eRT setv Device.LogAgent.WifiLogMsg string "RDK_LOG_ERROR,RDKB_PROCESS_CRASHED : WiFiAgent_process is not running, need restart"
 		$BINPATH/CcspWifiSsp -subsys $Subsys &
-	fi
+	else
+		APUP_PID=`pidof apup`
+		if [ "$APUP_PID" == "" ]; then
+			check_ap_enable5=`cfg -e | grep AP_ENABLE_2=1 | cut -d"=" -f2`
+			check_radio_enable5=`cfg -e | grep AP_RADIO_ENABLED=1 | cut -d"=" -f2`
+			check_interface_up5=`ifconfig | grep ath1`
+			check_ap_enable2=`cfg -e | grep AP_ENABLE=1 | cut -d"=" -f2`
+                        check_radio_enable2=`cfg -e | grep AP_RADIO_ENABLED_2=1 | cut -d"=" -f2`
+                        check_interface_up2=`ifconfig | grep ath0`
 
-	check_ap_enable=`cfg -e | grep AP_ENABLE_2=1 | cut -d"=" -f2`
-	check_interface_up=`ifconfig | grep ath1 | cut -d"=" -f2`
 
-	if [ "$check_ap_enable" == "1" ] && [ "$check_interface_up" == "" ]
-	then
-		dmcli eRT setv Device.LogAgent.WifiLogMsg string "[RKDB_PLATFORM_ERROR]: ath1 is down, restarting WiFi"
-		dmcli eRT setv Device.X_CISCO_COM_DeviceControl.RebootDevice string Wifi
-	fi
-
-	HOSTAPD_PID=`pidof hostapd`
-	if [ "$HOSTAPD_PID" = "" ]; then
-		echo "Hostapd process is not running, restarting it"
-                dmcli eRT setv Device.LogAgent.WifiLogMsg string "RDK_LOG_ERROR,RDKB_PROCESS_CRASHED : Hostapd_process is not running, need restart"
-                killall hostapd
-                hostapd  -B `cat /tmp/conf_filename` -e /nvram/etc/wpa2/entropy -P /tmp/hostapd.pid 1>&2
-
+			if [ "$check_ap_enable5" == "1" ] && [ "$check_radio_enable5" == "1" ] && [ "$check_interface_up5" == "" ]; then
+				echo "ath1 is down, restarting WiFi"
+				dmcli eRT setv Device.LogAgent.WifiLogMsg string "[RKDB_PLATFORM_ERROR]: ath1 is down, restarting WiFi"
+				dmcli eRT setv Device.X_CISCO_COM_DeviceControl.RebootDevice string Wifi
+			elif [ "$check_ap_enable2" == "1" ] && [ "$check_radio_enable2" == "1" ] && [ "$check_interface_up2" == "" ]; then
+                                echo "ath0 is down, restarting WiFi"
+                                dmcli eRT setv Device.LogAgent.WifiLogMsg string "[RKDB_PLATFORM_ERROR]: ath0 is down, restarting WiFi"
+                                dmcli eRT setv Device.X_CISCO_COM_DeviceControl.RebootDevice string Wifi
+                        else
+				HOSTAPD_PID=`pidof hostapd`
+				if [ "$HOSTAPD_PID" = "" ]; then
+				echo "Hostapd process is not running, restarting WiFi"
+                		dmcli eRT setv Device.LogAgent.WifiLogMsg string "RDK_LOG_ERROR,RDKB_PROCESS_CRASHED : Hostapd_process is not running, restarting WiFi"
+				dmcli eRT setv Device.X_CISCO_COM_DeviceControl.RebootDevice string Wifi
+				fi
+			fi
+		fi
 	fi
 
         cd /usr/ccsp/harvester
