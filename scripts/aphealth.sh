376b68d382d232262f3cb227287d87111597cb17
Zhicheng <Zhicheng_qiu@cable.comcast.com>
2016-10-10 17:04:37 -0700

    RDKB-8145: wifi telemetry log
    Reason for change: feature developing
    Test Procedure: run /usr/ccsp/wifi/aphealth.sh and stahealth.sh from atom console
    Risks: None
    Signed-off-by: Zhicheng <Zhicheng_qiu@comcast.com>
    
    Change-Id: I2731ee87fb3b4efd99d64402657141ab104353dc

--- scripts/aphealth.sh
+++ scripts/aphealth.sh
@@ -1,12 +1,48 @@
 #!/bin/sh
 #This script is used to log parameters for each AP
-tm=`date "+%s"`
-echo "AP $tm {"
-ifconfig | grep ath | cut -d' ' -f1 | while read ap; do
-    rx=`ifconfig $ap | grep "RX bytes" | cut -d':' -f2 | cut -d' ' -f1`
-    if [ "$rx" != 0 ]; then
-        echo "$ap RX:$rx"
-    fi
-done
-echo "}"
+ssid1="ath0"
+ssid2="ath1"
+logfolder="/tmp/wifihealth"
+oneMB=1048576;
 
+if [ ! -d "$logfolder" ] ; then
+	mkdir "$logfolder";
+fi
+
+rx1_0=`cat "$logfolder/rx1_0"`
+rx2_0=`cat "$logfolder/rx2_0"`
+
+if [ "$rx1_0" == "" ] ; then
+	rx1_0=0;
+fi
+if [ "$rx2_0" == "" ] ; then
+        rx2_0=0;
+fi
+
+rx1=`ifconfig "$ssid1" | grep "RX bytes" | cut -d':' -f2 | cut -d' ' -f1`
+rx2=`ifconfig "$ssid2" | grep "RX bytes" | cut -d':' -f2 | cut -d' ' -f1`
+
+if [ "$rx1" == "" ] ; then
+        rx1=0;
+fi
+if [ "$rx2" == "" ] ; then
+        rx2=0;
+fi
+
+echo "$rx1" > $logfolder/rx1_0;
+echo "$rx2" > $logfolder/rx2_0;
+
+rx1d=$(($rx1-$rx1_0))
+rx2d=$(($rx2-$rx2_0))
+
+rx1_mb=$(($rx1/$oneMB))
+rx2_mb=$(($rx2/$oneMB))
+
+rx1d_mb=$(($rx1d/$oneMB))
+rx2d_mb=$(($rx1d/$oneMB))
+
+echo "WIFI_RX_1:$rx1_mb"
+echo "WIFI_RXDELTA_1:$rx1d_mb"
+
+echo "WIFI_RX_2:$rx2_mb"
+echo "WIFI_RXDELTA_2:$rx2d_mb"
--- scripts/stahealth.sh
+++ scripts/stahealth.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 #This script is used to log parameters for each AP
-tm=`date "+%s"`
-ifconfig | grep ath | cut -d' ' -f1 | while read ap; do
-    sta=`wlanconfig $ap list sta | grep -v ADDR | tr -s " " | awk '{$6=$6-95; print}'`
-    if [ "$sta" != "" ]; then
-        echo "STA $ap $tm {"
-        echo $sta;
-        echo "}"
-    fi
-done
+ssid1="ath0"
+ssid2="ath1"
+
+sta1=`wlanconfig $ssid1 list sta | grep -v ADDR | tr -s " " | awk '{$6=$6-95; print}' | cut -d' ' -f1,4,5,6`
+sta2=`wlanconfig $ssid2 list sta | grep -v ADDR | tr -s " " | awk '{$6=$6-95; print}' | cut -d' ' -f1,4,5,6`
+
+if [ "$sta1" != "" ] ; then
+	mac1=`echo "$sta1" | cut -d' ' -f1 | tr '\n' ','`
+	echo "WIFI_MAC_1:$mac1"
+	rssi1=`echo "$sta1" | cut -d' ' -f4 | tr '\n' ','`
+	echo "WIFI_RSSI_1:$rssi1"
+	rxrate1=`echo "$sta1" | cut -d' ' -f3 | tr '\n' ','`
+	echo "WIFI_RXCLIENTS_1:$rxrate1"
+	txrate1=`echo "$sta1" | cut -d' ' -f2 | tr '\n' ','`
+	echo "WIFI_TXCLIENTS_1:$txrate1"
+
+	echo "$sta1" | cut -d' ' -f3 | tr -d 'M' > /tmp/rxx1
+	echo "$sta1" | cut -d' ' -f2 | tr -d 'M' > /tmp/txx1
+	rxtxd1=`awk '{printf ("%s ", $0); getline < "/tmp/txx1"; print $0 }' /tmp/rxx1 | awk '{print ($1 - $2)}' |  tr '\n' ','`
+	rm /tmp/rxx1 /tmp/txx1
+	echo "WIFI_RXTXCLIENTDELTA_1:$rxtxd1"
+fi
+
+if [ "$sta2" != "" ] ; then
+        mac2=`echo "$sta2" | cut -d' ' -f1 | tr '\n' ','`
+        echo "WIFI_MAC_2:$mac2"
+        rssi2=`echo "$sta2" | cut -d' ' -f4 | tr '\n' ','`
+        echo "WIFI_RSSI_2:$rssi2"
+        rxrate2=`echo "$sta2" | cut -d' ' -f3 | tr '\n' ','`
+        echo "WIFI_RXCLIENTS_2:$rxrate2"
+        txrate2=`echo "$sta2" | cut -d' ' -f2 | tr '\n' ','`
+        echo "WIFI_TXCLIENTS_2:$txrate2"
+
+	echo "$sta2" | cut -d' ' -f3 | tr -d 'M' > /tmp/rxx2
+        echo "$sta2" | cut -d' ' -f2 | tr -d 'M' > /tmp/txx2
+        rxtxd2=`awk '{printf ("%s ", $0); getline < "/tmp/txx2"; print $0 }' /tmp/rxx2 | awk '{print ($1 - $2)}' |  tr '\n' ','`
+	rm /tmp/rxx2 /tmp/txx2
+        echo "WIFI_RXTXCLIENTDELTA_2:$rxtxd2"
+fi
+
 
