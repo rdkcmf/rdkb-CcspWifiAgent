e050a967e52a9265798bb11b0aeb0446f8e2a16b
kiran biswal <kiran_biswal@cable.comcast.com>
2017-03-07 00:45:32 +0000

    ARRISXB3-5651 : cron job race condition protection
    Reason for change: added checks
    
    Test Procedure:
    
    connect wifi clients
    wait for more than 1 hour
    check if /rdklogs/logs folder
    has wifihealth.txt with all the expected data
    check atom console log has top output printed every hour
    
    try enabling hotspot and disabling hotspot several times and see if atom is hung
    
    check if atom is hung(cli commands fail and load average is high)
    
    Risks: NA
    
    Change-Id: I105473188afbc0eb5a20278eafbeff95c207bfb2
    Signed-off-by: kiran biswal <kiran_biswal@cable.comcast.com>

--- scripts/stahealth_log.sh
+++ scripts/stahealth_log.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-/usr/ccsp/wifi/stahealth.sh >> /rdklogs/logs/wifihealth.txt
+uptime=`cat /proc/uptime | awk '{ print $1 }' | cut -d"." -f1`
+echo "before running stahealth.sh printing top output" >> /rdklogs/logs/AtomConsolelog.txt
+top -n1 >> /rdklogs/logs/AtomConsolelog.txt
+if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof hostapd)" != "" ]  && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof radiohealth_log.sh)" == "" ] && [ "$(pidof stahealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof bandsteering_log.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ]  && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] ; then
+        /usr/ccsp/wifi/stahealth.sh >> /rdklogs/logs/AtomConsolelog.txt
+        echo "after running stahealth.sh printing top output" >> /rdklogs/logs/AtomConsolelog.txt
+        top -n1 >> /rdklogs/logs/AtomConsolelog.txt
+else
+        echo "skipping staheath.sh run" >> /rdklogs/logs/AtomConsolelog.txt
+	
+fi
+
 
