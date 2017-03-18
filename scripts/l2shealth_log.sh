a7bb9f5fa9ea7e99ea491524da1b0a3bf7a71e35
kiran biswal <kiran_biswal@cable.comcast.com>
2017-03-18 00:36:38 +0000

    RDKB-8966: unnecessary log removal and cleanup of process monitor
    Reason for change: cleanup
    
    Test Procedure:
    
    Connect several wifi clients (as many as possible unto 10 including iPhone and MacBooks) and leave it for an hour
    see that wifihealth.txt has valid data
    check that AtomConsolelog.txt.0 does not have DEAUTH messages flooded
    kill and wifiagent and hostapd
    see if they are automatically coming up after about 10 minutes
    
    check that when wifi restore default is done both 2g and 5g ssids are commited and reflect in gui
    
    Change-Id: I205e3b214a66526ef16a31177405f7abb62d8084
    
    Risks: NA
    
    Signed-off-by: kiran biswal <kiran_biswal@cable.comcast.com>
    Change-Id: I205e3b214a66526ef16a31177405f7abb62d8084

--- scripts/l2shealth_log.sh
+++ scripts/l2shealth_log.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 uptime=`cat /proc/uptime | awk '{ print $1 }' | cut -d"." -f1`
-echo "before running l2shealth.sh printing top output" >> /rdklogs/logs/AtomConsolelog.txt
-top -n1 >> /rdklogs/logs/AtomConsolelog.txt
-if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof hostapd)" != "" ]  && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof radiohealth_log.sh)" == "" ] && [ "$(pidof stahealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof bandsteering_log.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ]  && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] ; then
+echo "before running l2shealth.sh printing top" >> /rdklogs/logs/AtomConsolelog.txt.0
+top -n1 >> /rdklogs/logs/AtomConsolelog.txt.0
+if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof hostapd)" != "" ]  && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ]  && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] ; then
 	/usr/ccsp/wifi/l2shealth.sh >> /rdklogs/logs/AtomConsolelog.txt.0
-        echo "after running l2shealth.sh printing top output" >> /rdklogs/logs/AtomConsolelog.txt.0
+        echo "top after running l2shealth.sh" >> /rdklogs/logs/AtomConsolelog.txt.0
         top -n1 >> /rdklogs/logs/AtomConsolelog.txt.0
 else
         echo "skipping l2shealth.sh run" >> /rdklogs/logs/AtomConsolelog.txt.0
 	
 fi
-
-
 
