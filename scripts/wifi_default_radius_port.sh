35e962db4dae18321106f07b904ac0dd92aa1f93
skolipaka <sudheep_kolipaka@comcast.com>
2017-06-08 00:42:46 +0000

    CISCOXB3-2450: Request to set port number MIb to 1812 by default
    Reason for change:Added a script wifi_default_radius_port.sh
    Test Procedure: build the image, flash the image,set the secure ssid, Factory reset,
    check for cfg -e | grep AP_AUTH_PORT command.
    Risks: None
    Signed-off-by: Sudheep kolipaka <sudheep_kolipaka@comcast.com>
    
    Change-Id: Icb7830a3acb37b3a6be33781fe286f1a6867cc0e

--- scripts/wifi_default_radius_port.sh
+++ scripts/wifi_default_radius_port.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # set 1812 as AP_AUTH_PORT default
 
-port=` cfg -e | grep AP_AUTH_PORT__9 | cut -d"=" -f2`
-if [ "$port" ==  "0" ]; then
+port=` cfg -e | grep AP_AUTH_PORT_9 | cut -d"=" -f2`
+if [[ "$port" ==  "0" || "$port" == "" ]]; then
       cfg -a AP_AUTH_PORT_7=1812
       cfg -a AP_AUTH_PORT_8=1812
       cfg -a AP_AUTH_PORT_9=1812
