48e18a610fed5449ac6243bafdfdb30301899451
skolipaka <sudheep_kolipaka@comcast.com>
2017-06-20 20:19:40 +0000

    CISCOXB3-2450: Request to set port number MIb to 1812 by default
    Reason for change:Modified script wifi_default_radius_port.sh
    Test Procedure: build the image, flash the image,
    check for cfg -s | grep AP_AUTH and SNMP commands.
    Risks: None
    Signed-off-by: Sudheep kolipaka <sudheep_kolipaka@comcast.com>
    
    Change-Id: I45a7293536e75ac181642a272739006caaa38962

--- scripts/wifi_default_radius_port.sh
+++ scripts/wifi_default_radius_port.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
-# set 1812 as AP_AUTH_PORT default
 
-port=` cfg -e | grep AP_AUTH_PORT_9 | cut -d"=" -f2`
-if [[ "$port" ==  "0" || "$port" == "" ]]; then
-      cfg -a AP_AUTH_PORT_7=1812
-      cfg -a AP_AUTH_PORT_8=1812
+DEVICE_MODEL=`grep DEVICE_MODEL /etc/device.properties | cut -d"=" -f2`
+if [ "$DEVICE_MODEL" != "TCHXB3" ]; then
+	exit 0;
+fi
+
+port2_9=` cfg -e | grep AP_AUTH_PORT2_9 | cut -d"=" -f2`
+port9=` cfg -e | grep AP_AUTH_PORT_9 | cut -d"=" -f2`
+port10=` cfg -e | grep AP_AUTH_PORT_10 | cut -d"=" -f2`
+port11=` cfg -e | grep AP_AUTH_PORT_11 | cut -d"=" -f2`
+port12=` cfg -e | grep AP_AUTH_PORT_12 | cut -d"=" -f2`
+
+if [[ "$port2_9" ==  "0" || "$port2_9" == "" ||
+      "$port9" ==  "0" || "$port9" == "" || 
+      "$port10" ==  "0" || "$port10" == "" ||
+      "$port11" ==  "0" || "$port11" == "" ||
+      "$port12" ==  "0" || "$port12" == "" ]]; then
       cfg -a AP_AUTH_PORT_9=1812
       cfg -a AP_AUTH_PORT_10=1812
-      cfg -a AP_AUTH_PORT_SECONDARY_7=1812
-      cfg -a AP_AUTH_PORT_SECONDARY_8=1812
-      cfg -a AP_AUTH_PORT_SECONDARY_9=1812
-      cfg -a AP_AUTH_PORT_SECONDARY_10=1812
+      cfg -a AP_AUTH_PORT_11=1812
+      cfg -a AP_AUTH_PORT_12=1812
+      cfg -a AP_AUTH_PORT2_9=1812
+      cfg -a AP_AUTH_PORT2_10=1812
+      cfg -a AP_AUTH_PORT2_11=1812
+      cfg -a AP_AUTH_PORT2_12=1812
       cfg -c
 fi
