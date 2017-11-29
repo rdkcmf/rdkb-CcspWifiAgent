8befed55407faffb4925e66d6e8aac5816270a69
pkondedath <Prashanth_Kondedath@comcast.com>
2017-11-29 23:45:20 +0000

    RDKB-14826,RDKB-13979: modified meshapcfg script
    Reason for change: do read and then write para
    Test Procedure: Verify if meshapcfg script
    properly configures the ath12 and ath13 para
    Risks: None
    Signed-off-by: Prashanth Kondedath <Prashanth_Kondedath@comcast.com>
    
    Change-Id: I78894ee6f784b7a21c4c22cb602be6351a7ff10d

--- scripts/meshapcfg.sh
+++ scripts/meshapcfg.sh
@@ -1,66 +1,68 @@
 # This script is used to config the SSID.13, SSID.14 for mesh backhaul GRE
 # by zhicheng_qiu@comcast.com
-#!/bin/sh
-#RDKB-13835: Sometimes race conditions prevents bringing up of ath12 and ath13
-wifi_api wifi_setApEnable 12 1
-wifi_api wifi_setApEnable 13 1
+# prash: modified script for writing the mesh para after reading the current value
 
-#MESH-223
-uapsd=`wifi_api wifi_getApWmmUapsdEnable 12 | head -n 1`
-if [ "$uapsd" != "FALSE" ]; then
-	wifi_api wifi_setApWmmUapsdEnable 12 0
-	wifi_api wifi_setApWmmUapsdEnable 13 0
-fi
-
-brname=`wifi_api wifi_getApBridgeInfo 12 "" "" "" | head -n 1`
-if [ "$brname" == "br12" ]; then
-	exit 0;
-fi
+MODEL_NUM=`grep MODEL_NUM /etc/device.properties | cut -d "=" -f2`
 
 for idx in 12 13
 do
 
-	if [ "$idx" == "12" ]; then
-		brname="br12"
-		vlan=112
-	else
-		brname="br13"
-		vlan=113
+        if [ "$idx" == "12" ]; then
+                brname="br12"
+                vlan=112
+        else
+                brname="br13"
+                vlan=113
+        fi
+
+        
+        uapsd=`wifi_api wifi_getApWmmUapsdEnable $idx | head -n 1`
+        if [ "$uapsd" != "FALSE" ]; then
+	 wifi_api wifi_setApWmmUapsdEnable $idx 0
 	fi
 
-	#AP_BRNAME_13:=.
-	wifi_api wifi_setApBridgeInfo  $idx $brname "" ""
+        #AP_BRNAME_13:=.
+        if [ `wifi_api wifi_getApBridgeInfo $idx "" "" "" | head -n 1` != "$brname" ]; then
+         wifi_api wifi_setApBridgeInfo  $idx $brname "" ""
+        fi
+        
+        wifi_api wifi_setApVlanID $idx $vlan
+        
+        if [ `wifi_api wifi_getApSsidAdvertisementEnable $idx` != "FALSE" ]; then
+         wifi_api wifi_setApSsidAdvertisementEnable $idx 0
+        fi
 
-	#AP_VLAN_13:=
-	wifi_api wifi_setApVlanID $idx $vlan
+        if [ `wifi_api wifi_getApBeaconType $idx` == "None" ]; then
+         wifi_api wifi_setApBeaconType $idx "WPAand11i"
+        fi
 
-	#AP_STATIONISOLATE_13:=0
-	#wifi_api wifi_setAPIsolationEnable $idx 0
+        #AP_SECFILE_13:=PSK
+        wifi_api wifi_setApBasicAuthenticationMode $idx "PSKAuthentication"
+      
+        if [ `wifi_api wifi_getApWpaEncryptionMode $idx` != "TKIPandAESEncryption" ]; then
+         wifi_api wifi_setApWpaEncryptionMode $idx "TKIPandAESEncryption"
+        fi
 
-	#AP_HIDESSID_13:=1
-	wifi_api wifi_setApSsidAdvertisementEnable   $idx 0
+        if [ `wifi_api wifi_getSSIDName $idx` != "we.piranha.off" ]; then
+          wifi_api wifi_setSSIDName $idx "we.piranha.off"
+        fi
+ 
+        #PSK_KEY_13:=welcome8
+        if [ $MODEL_NUM == "DPC3941" ] || [ $MODEL_NUM == "TG1682G" ]; then
+         if [ -z `wifi_api wifi_getApSecurityPreSharedKey $idx` ]; then
+          wifi_api wifi_setApSecurityPreSharedKey $idx "welcome8"
+         fi
+        else
+          wifi_api wifi_setApSecurityPreSharedKey $idx "welcome8"
+        fi
 
-	#AP_SECMODE_13:=WPA
-	#AP_WPA_13:=3
-	wifi_api wifi_setApBeaconType $idx "WPAand11i"
-
-	#AP_SECFILE_13:=PSK
-	wifi_api wifi_setApBasicAuthenticationMode $idx "PSKAuthentication"
-
-	#AP_CYPHER_13:=TKIP CCMP
-	wifi_api wifi_setApWpaEncryptionMode $idx "TKIPandAESEncryption"
-
-	#AP_SSID_13:=we.piranha
-	wifi_api wifi_setSSIDName $idx "we.piranha.off"
-
-	#PSK_KEY_13:=welcome8
-	wifi_api wifi_setApSecurityPreSharedKey $idx "welcome8"
-
-	#WPS_ENABLE_13:=0
-	wifi_api wifi_setApWpsEnable $idx 0
-
-	#AP_ENABLE_13:=1
-	wifi_api wifi_setApEnable  $idx 1
-
+        if [ `wifi_api wifi_getApWpsEnable $idx` != "FALSE" ]; then
+         wifi_api wifi_setApWpsEnable $idx 0
+        fi
+  
+        if [ `wifi_api wifi_getApEnable $idx` != "TRUE" ]; then
+         wifi_api wifi_setApEnable  $idx 1
+        fi          
+                
 done
 
