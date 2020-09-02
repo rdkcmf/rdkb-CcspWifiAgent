a29ca033691306235fd6ff1807feb54fa74777a0
Harikrishnan Raman <Harikrishnan_Raman@comcast.com>
2020-09-02 10:09:49 +0000

    RDKB-30386 RDKB-29536 BS status is not updated on BS periodic status logs
    Reason for change:
    bandsteering.sh is not used in non XB3 devices
    Test Procedure:
    Trigger "aphealth_log.sh" and no errors should be seen
    Bandsteering enabled status should be updated on "bandsteering_periodic_status.txt" logs
    Risks: Low
    Signed-off-by:Harikrishnan_Raman@comcast.com
    
    RDKB-29536 Bandsteering output formatted
    
    Reason for change:
    Bandsteering wifiapi returns "1/0" for QTN
    "TRUE/FALSE" for BRCM
    
    Impacted Platforms:
    XB6, XF3, CBR, TXB7
    
    Test Procedure:
    Trigger "aphealth_log.sh" and no errors should be seen
    Bandsteering enabled status should be updated on "bandsteering_periodic_status.txt" logs
    
    Risks: Low
    Signed-off-by:Harikrishnan_Raman@comcast.com
    
    Change-Id: I1b3e54501161d18411d47b2f35ecbe67771363f6

--- scripts/aphealth_log.sh
+++ scripts/aphealth_log.sh
@@ -23,32 +23,45 @@ then
     source /etc/device.properties
 fi
 
+if [ -e /etc/log_timestamp.sh ]
+then
+    source /etc/log_timestamp.sh
+else
+    echo_t()
+    {
+        echo "`date +"%y%m%d-%T.%6N"` $1"
+    }
+fi
+
 uptime=`cat /proc/uptime | awk '{ print $1 }' | cut -d"." -f1`
-echo "before running aphealth.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
+echo_t "before running aphealth.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
 top -n1 > /tmp/top.txt
 cat /tmp/top.txt >> $WIFI_CONSOLE_LOG_NAME
 if [ "$BOX_TYPE" = "XB3" ]; then
 	if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof hostapd)" != "" ]  && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof radiohealth_log.sh)" == "" ] && [ "$(pidof stahealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof bandsteering_log.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ]  && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] && [ "$(pidof dailystats.sh)" == "" ] && [ "$(pidof dailystats_log.sh)" == "" ] ; then
                 /usr/ccsp/wifi/aphealth.sh >> /rdklogs/logs/wifihealth.txt
-                #/usr/ccsp/wifi/stahealth.sh >> /rdklogs/logs/wifihealth.txt  #zqiu: start from cosa_start.sh or systemd
                 /usr/ccsp/wifi/bandsteering.sh >> /rdklogs/logs/bandsteering_periodic_status.txt
-                echo "after running aphealth.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
+                echo_t "after running aphealth.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
                 top -n1 > /tmp/top.txt
                 cat /tmp/top.txt>> $WIFI_CONSOLE_LOG_NAME
         else
-                echo "skipping apheath.sh run" >> $WIFI_CONSOLE_LOG_NAME
+                echo_t "skipping apheath.sh run" >> $WIFI_CONSOLE_LOG_NAME
         fi
 else
 	if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] && [ "$(pidof dailystats.sh)" == "" ] && [ "$(pidof dailystats_log.sh)" == "" ] ; then
                 /usr/ccsp/wifi/aphealth.sh >> /rdklogs/logs/wifihealth.txt
-                #/usr/ccsp/wifi/stahealth.sh >> /rdklogs/logs/wifihealth.txt  #zqiu: start from cosa_start.sh or systemd
-                /usr/ccsp/wifi/bandsteering.sh >> /rdklogs/logs/bandsteering_periodic_status.txt
-                echo "after running aphealth.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
+                buf=`wifi_api wifi_getBandSteeringEnable`
+                if [[ "$buf" == "TRUE" || "$buf" == "1" ]]; then
+                        buf="true"
+                else
+                        buf="false"
+                fi
+                echo_t "BANDSTEERING_ENABLE_STATUS:$buf"  >> /rdklogs/logs/bandsteering_periodic_status.txt
+                echo_t "after running aphealth.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
                 top -n1 > /tmp/top.txt
                 cat /tmp/top.txt>> $WIFI_CONSOLE_LOG_NAME
         else
-                echo "skipping apheath.sh run" >> $WIFI_CONSOLE_LOG_NAME
+                echo_t "skipping apheath.sh run" >> $WIFI_CONSOLE_LOG_NAME
         fi
 fi
-
 
