a0a19d43f2b8ec8242007998f39ecae4f3ed2ca4
Harikrishnan Raman <Harikrishnan_Raman@comcast.com>
2020-09-27 11:53:12 +0000

    RDKB-32000, RDKB-31894 RDKB-31885 normalized rssi not reported
    Impacted Platforms:
    All RDKB Boxes
    
    Reason for change:
    remove station health scripts
    
    Test Procedure:
    connect client on both radios
    normalized rssi markers to be captured in wifihealth logs
    psmcli with "1,2" should return to 1,2
    
    Risks: High
    
    Signed-off-by:Harikrishnan_Raman@comcast.com
    
    Change-Id: I844653c2ef5bd7137ead546e530a4e5bc4462876
    Signed-off-by:Harikrishnan_Raman@comcast.com
    
    RDKB-32000 remove station health scripts
    
    Impacted Platforms:
    All RDKB Boxes
    
    Reason for change:
    remove station health scripts
    
    Test Procedure:
    Test Procedure:
    1. Below 3 files should not be there in the device
               stahealth.sh
               stahealth_log.sh
               wifi-telemetry-sta.service
    2. No errors should be there releated to the above 3 files
    
    Risks: High
    
    Change-Id: I3d57f2f7b34f16d46c6b2bb0465c1d8ca818f1b6
    Signed-off-by: Harikrishnan_Raman@comcast.com
    (cherry picked from commit 9a9ee228c6123147d235d8e9210d86c9618077f3)

--- scripts/dailystats_log.sh
+++ scripts/dailystats_log.sh
@@ -28,7 +28,7 @@ uptime=`cat /proc/uptime | awk '{ print $1 }' | cut -d"." -f1`
 echo "before running dailystats.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
 top -n1 >> $WIFI_CONSOLE_LOG_NAME
 if [ "$BOX_TYPE" = "XB3" ]; then
-	if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof hostapd)" != "" ]  && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof radiohealth_log.sh)" == "" ] && [ "$(pidof stahealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof bandsteering_log.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ]  && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] ; then
+	if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof hostapd)" != "" ]  && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof radiohealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof bandsteering_log.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ]  && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] ; then
 			/usr/ccsp/wifi/dailystats.sh >> /rdklogs/logs/wifihealth.txt
 			echo "after running dailystats.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
 			top -n1 >> $WIFI_CONSOLE_LOG_NAME
@@ -37,7 +37,7 @@ if [ "$BOX_TYPE" = "XB3" ]; then
 
 	fi
 else
-	if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] && [ "$(pidof dailystats.sh)" == "" ] ; then
+	if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof log_mem_cpu_info_atom.sh)" == "" ] && [ "$(pidof dailystats.sh)" == "" ] ; then
 			/usr/ccsp/wifi/dailystats.sh >> /rdklogs/logs/wifihealth.txt
 			echo "after running dailystats.sh printing top output" >> $WIFI_CONSOLE_LOG_NAME
 			top -n1 >> $WIFI_CONSOLE_LOG_NAME
