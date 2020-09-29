35f85b42e67b687eb4c0f9a82189def96bc247e7
ssathy004c <Sajeesh_Sathyan2@comcast.com>
2020-09-29 02:06:19 +0000

    RDKB-31924 : Re-use elxrretyt.swr
    Reason for change: To reduce the usage of CEDM.
    Do not remove the GetConfigFile output for elxrretyt.swr
    Call GetConfigFile only if elxrretyt.swr not exist.
    Test Procedure: check the presence of usage.log
    Risks: Low
    Signed-off-by: sajeesh_sathyan2@comcast.com
    Change-Id: Ibacd5da956fd37ead1b43f8c12dc1f4092695c67

--- scripts/synclease.sh
+++ scripts/synclease.sh
@@ -21,5 +21,7 @@
 #for the first time when mesh is enabled, also during plume agent
 #restart
 ARM_IP=`grep "ARM_INTERFACE_IP" /etc/device.properties | cut -d "=" -f2`
-configparamgen jx /etc/dropbear/elxrretyt.swr /tmp/login.swr
+if[ ! -f /tmp/login.swr ]; then
+    configparamgen jx /etc/dropbear/elxrretyt.swr /tmp/login.swr
+fi
 scp -i /tmp/login.swr root@$ARM_IP:/nvram/dnsmasq.leases /tmp/dnsmasq.leases
