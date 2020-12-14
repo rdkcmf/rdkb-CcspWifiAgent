61041f97811db5b67d9ef77b66a5fbb4effc9776
sportl986 <Steven_Portley@comcast.com>
2020-12-14 23:48:48 +0000

    RDKB-29433 - Logs when backhaul SSID defaults for Pod Customers
    Reason for change: Add different log statements for ath12/ath13
    Test Procedure: Set pod SSID to default, make sure it's eventually logged
    Risks: Low
    Signed-off-by: <Steven_Portley@comcast.com>
    
    (cherry picked from commit b76e72589e48e15ac0520939eb78f24ca5c2378e)
    Change-Id: I7ed3440fa6b78a348629f954bce65ffc9dd18483

--- scripts/mesh_status.sh
+++ scripts/mesh_status.sh
@@ -103,3 +103,16 @@ else
   echo_t "pod_detected:unsupported_port" >> /rdklogs/logs/MeshAgentLog.txt.0                                  
  fi                                                                       
 fi  
+
+VIF_CONFIG_CHECK=`/usr/plume/tools/ovsh s Wifi_VIF_Config -w ssid==we.connect.yellowstone`
+if [ "$VIF_CONFIG_CHECK" != "" ] && [ $pod_mac != "" ]; then
+    SSID_NAME_12=`wifi_api wifi_getSSIDName 12`
+    SSID_NAME_13=`wifi_api wifi_getSSIDName 13`
+    if [ "$SSID_NAME_12" != "we.connect.yellowstone" ]; then
+        echo_t "2G SSID is still default for pod customer" >> /rdklogs/logs/MeshAgentLog.txt.0
+    fi
+       
+    if [ "$SSID_NAME_13" != "we.connect.yellowstone" ]; then
+        echo_t "5G SSID is still default for pod customer" >> /rdklogs/logs/MeshAgentLog.txt.0
+    fi
+fi
