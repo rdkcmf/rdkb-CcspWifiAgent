029f10e8d370542ff7f1089e54da6901d0d8e3cd
Prabhudas Gannavarapu <prabhudas.gannavarapu@sky.uk>
2021-02-25 16:52:32 +0000

    SKYH4-3675 : Need to support 11kv feature for HUB4 LO7 WLDM code base.
    Reason for change:
    Enable 11kv feature for HUB4 WLDM platform. TR181 should allow for WLDM product but LO3 code base should remain same.
    
    Test Procedure:
    1. 11kv feature should work without any issue
    2. Below two TR181 params can be configure via RFC,
    Device.WiFi.AccessPoint.{I}.X_RDKCENTRAL-COM_BSSTransitionActivatedDevice.WiFi.AccessPoint.{I}.X_RDKCENTRAL-COM_NeighborReportActivated
    
    Risks: Low
    
    Signed-off-by: Lakshminarayanan Shenbagaraj <lakshminarayanan_shenbagaraj@comcast.com>
    (cherry picked from commit 551dc3c81e11ee7088223befd47133c14e1ca1a1)
    Change-Id: I2587a5459fa7f9bd2ed543792eaaa1f8939041d8

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -6965,7 +6965,7 @@ AccessPoint_GetParamBoolValue
     if (AnscEqualString(ParamName, "X_RDKCENTRAL-COM_NeighborReportActivated", TRUE))
     {
         /* collect value */
-#if defined(_COSA_BCM_MIPS_) || defined(_CBR_PRODUCT_REQ_)
+#if defined(_COSA_BCM_MIPS_) || defined(_CBR_PRODUCT_REQ_) || defined(HUB4_WLDM_SUPPORT)
         INT wlanIndex = -1;
         wlanIndex = pWifiAp->AP.Cfg.InstanceNumber -1 ;
         wifi_getNeighborReportActivation(wlanIndex , &pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated);
