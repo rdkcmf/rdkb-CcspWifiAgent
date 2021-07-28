dd87e6ea998aa2222d0e5994a09700beffe45404
Manish_Dharanenthiran <Manish_Dharanenthiran@Comcast.com>
2021-07-28 05:22:24 +0000

    RDKB-36751 : Compliance issues in wifiagent files
    Reason for change:
     - RDKB Apache headers and license information
       is absent in some of the Wifiagent files
    Test Procedure:
     - Test with scanning tool and make sure
       there are no compliance errors
    Risks: Low
    
    Signed-off-by: Manish_Dharanenthiran <Manish_Dharanenthiran@Comcast.com>
    (cherry picked from commit 8d7b22b4efbf64e0a16e8b3dbe9ce885a9219e1f)
    Change-Id: I4db04e00c049ac7ac0310a76dcc6a8dba047a1b6

--- source/TR-181/sbapi/wifi_hostap_auth.h
+++ source/TR-181/sbapi/wifi_hostap_auth.h
@@ -1,3 +1,33 @@
+/************************************************************************************
+  If not stated otherwise in this file or this component's Licenses.txt file the
+  following copyright and licenses apply:
+
+  Copyright 2021 RDK Management
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+**************************************************************************/
+
+/*************************************************************************
+Some material from:
+  Copyright (c) 2010, Atheros Communications Inc.
+  Licensed under the ISC License
+and
+  Copyright (c) 2001 Atsushi Onoe
+  Copyright (c) 2002-2005 Sam Leffler, Errno Consulting
+  All rights reserved.
+  Licensed under the BSD-3 License
+**************************************************************************/
+
 #include "utils/includes.h"
 #include "utils/common.h"
 #include "utils/eloop.h"
