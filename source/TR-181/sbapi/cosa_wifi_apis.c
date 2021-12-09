a761b851dcda8d677f59b71f03cf5ec026325437
jmathe214 <jino_mathew@comcast.com>
2021-12-09 09:37:16 +0000

    TCCBR-5796: Wi-Fi set Not working
    Reason for change: Wifi Set not working. wifi_apply not called.
    Test Procedure: dmcli set of wifi/Radio should work.
    Risks: Low
    Signed-off-by: Jino Mathew <Jino_Mathew@comcast.com>
    
    (cherry picked from commit 571a26e43bd6a84b84833a2239ede69f120aa6ae)
    Change-Id: I3bcdb886be1359e26c54cb189365ad41e676c22c

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -15564,7 +15564,7 @@ fprintf(stderr, "----# %s %d gRadioRestartRequest=%d %d \n", __func__, __LINE__,
 		}
 #endif
 		//<<
-#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_)
+#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_) || defined(_CBR_PRODUCT_REQ_)
 	       /* Converting brcm patch to code and this code will be removed as part of Hal Version 3 changes */
                if (wlanRestart) {
                        wifiDbgPrintf("### %s: ignore wlanRestart for instance %lu ###\n", __FUNCTION__, pCfg->InstanceNumber - 1);
@@ -15666,7 +15666,7 @@ fprintf(stderr, "----# %s %d gRadioRestartRequest=%d %d \n", __func__, __LINE__,
 		}
 #endif
 
-#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_)
+#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_) || defined(_CBR_PRODUCT_REQ_)
 	/* Converting brcm patch to code and this code will be removed as part of Hal Version 3 changes */
 	fprintf(stderr, "%s: calling wifi_apply()...\n", __func__);
         wifi_apply();
@@ -16630,7 +16630,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
     else
         returnStatus = ANSC_STATUS_FAILURE;
 
-#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_)
+#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_) || defined(_CBR_PRODUCT_REQ_)
     /* Converting brcm patch to code and this code will be removed as part of Hal Version 3 changes */
     fprintf(stderr, "%s: calling wifi_apply()\n", __func__);
     wifi_apply();
@@ -20916,7 +20916,7 @@ CosaDmlWiFiApMfSetCfg
 #endif /* _ENABLE_BAND_STEERING_ */
 	}
 
-#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_)
+#if (defined(_COSA_BCM_ARM_) && defined(_XB7_PRODUCT_REQ_)) || defined(_XB8_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_) || defined(_CBR_PRODUCT_REQ_)
     /* Converting brcm patch to code and this code will be removed as part of Hal Version 3 changes */
     fprintf(stderr, "%s: calling wifi_apply()...\n", __func__);
     wifi_apply();
