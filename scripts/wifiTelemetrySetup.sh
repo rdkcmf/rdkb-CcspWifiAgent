59c5574f6d6bc4f998bf88b6fa29b12b9a081a2f
aadroj583 <Ankur_Adroja@comcast.com>
2022-06-13 14:07:30 +0000

    RDKB-41162 : systemd phase 2 for CBR
    Reason for change: systemd phase 2 for CBR
    
    Test Procedure:tested
    
    Risks: None
    Signed-off-by: Ankur Adroja <Ankur_Adroja@comcast.com>
    (cherry picked from commit b9d2801ea2c8b1cfed139800f44fac61bd6330ff)
    Change-Id: Iafaf2d286a04634f559a4fda719903d7d460079a

--- scripts/wifiTelemetrySetup.sh
+++ scripts/wifiTelemetrySetup.sh
@@ -37,39 +37,83 @@ then
     source /etc/device.properties
 fi
 
-CRONFILE=$CRON_SPOOL"/root"
-CRONFILE_BK="/tmp/cron_tab$$.txt"
-ENTRY_ADDED=0
-
-echo "Start monitoring wifi system statistics"
-if [ -f $CRONFILE ]
+if [ "$IS_BCI" != "yes" ];
 then
-	# Dump existing cron jobs to a file & add new job
-	crontab -l -c $CRON_SPOOL > $CRONFILE_BK
-	
-	# Check whether specific cron jobs are existing or not
-	existing_aphealth=$(grep "aphealth_log.sh" $CRONFILE_BK)
-	existing_dailystats=$(grep "dailystats_log.sh" $CRONFILE_BK)
-	existing_radiostats=$(grep "radiohealth.sh" $CRONFILE_BK)
-	
-	if [ -z "$existing_aphealth" ]; then
-		echo "35 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE_BK
-		ENTRY_ADDED=1
-	fi
-	
-	if [ -z "$existing_dailystats" ]; then
-		echo "47 2 * * *  /usr/ccsp/wifi/dailystats_log.sh" >> $CRONFILE_BK
-		ENTRY_ADDED=1
-	fi
-	if [ -z "$existing_radiostats" ]; then
-		echo "*/15 * * * * sh /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE_BK
-		ENTRY_ADDED=1
-	fi
-	
-	
-	if [ $ENTRY_ADDED -eq 1 ]; then
-		crontab $CRONFILE_BK -c $CRON_SPOOL
-	fi
-	
+	CRONFILE=$CRON_SPOOL"/root"
+	CRONFILE_BK="/tmp/cron_tab$$.txt"
+	ENTRY_ADDED=0
+
+	echo "Start monitoring wifi system statistics"
+	if [ -f $CRONFILE ]
+	then
+		# Dump existing cron jobs to a file & add new job
+		crontab -l -c $CRON_SPOOL > $CRONFILE_BK
+
+		# Check whether specific cron jobs are existing or not
+		existing_aphealth=$(grep "aphealth_log.sh" $CRONFILE_BK)
+		existing_dailystats=$(grep "dailystats_log.sh" $CRONFILE_BK)
+		existing_radiostats=$(grep "radiohealth.sh" $CRONFILE_BK)
+
+		if [ -z "$existing_aphealth" ]; then
+			echo "35 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE_BK
+			ENTRY_ADDED=1
+		fi
+
+		if [ -z "$existing_dailystats" ]; then
+			echo "47 2 * * *  /usr/ccsp/wifi/dailystats_log.sh" >> $CRONFILE_BK
+			ENTRY_ADDED=1
+		fi
+        
+		if [ -z "$existing_radiostats" ]; then
+			echo "*/15 * * * * sh /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE_BK
+			ENTRY_ADDED=1
+		fi
+
+		if [ $ENTRY_ADDED -eq 1 ]; then
+			crontab $CRONFILE_BK -c $CRON_SPOOL
+		fi
 	rm -rf $CRONFILE_BK
+	fi
+else
+	CRONPATH="/var/spool/cron/crontabs/"
+	CRONFILE=$CRONPATH"root"
+	CRONFILE_BK="/tmp/cron_tab.txt"
+
+	echo "Start monitoring system statistics"
+	CRON_PID=`pidof crond`
+	if [ "$CRON_PID" = "" ]
+	then
+    		if [ -f $CRONFILE ]
+    		then
+        		grep -nr "/usr/ccsp/wifi/radiohealth.sh" $CRONFILE
+        		if [ $? -ne 0 ]; then
+            			echo "*/15 * * * *  /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE
+        		fi
+        		grep -nr "/usr/ccsp/wifi/aphealth_log.sh" $CRONFILE
+        		if [ $? -ne 0 ]; then
+            			echo "1 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE
+         		fi
+    		else
+        		if [ ! -d $CRONPATH ]
+        		then
+            			mkdir $CRONPATH
+        		fi
+				touch $CRONFILE
+        		echo "*/15 * * * *  /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE
+        		echo "1 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE
+    		fi
+    		crond -c $CRONPATH -l 9
+	else
+    		crontab -l -c $CRONPATH > $CRONFILE_BK
+    		grep -nr "/usr/ccsp/wifi/radiohealth.sh" $CRONFILE_BK
+    		if [ $? -ne 0 ]; then
+        		echo "*/15 * * * *  /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE_BK
+    		fi
+    			grep -nr "/usr/ccsp/wifi/aphealth_log.sh" $CRONFILE_BK
+    		if [ $? -ne 0 ]; then
+        		echo "1 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE_BK
+    		fi
+    		crontab $CRONFILE_BK -c $CRONPATH
+    		rm -rf $CRONFILE_BK
+	fi
 fi
